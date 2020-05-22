6096a22e2f328d5ddfef99ebd6ef7503c2e8d333
muscleboy <1076146831@qq.com>
2020-05-22 19:22:10 +0800

    new proj

---
+++ src/api/api.js
@@ -0,0 +1,44 @@
+import {
+  wxRequest
+} from '../utils/wxRequest';
+
+const API = "http://localhost:8000/api";
+
+/**
+ * 获取发现好商品接口
+ * @param  {[type]} params [description]
+ * @return {[type]}        [description]
+ */
+
+//微信的jscode换取sessionKey
+const wxJsCode2Session = (params) => wxRequest(params, API + "/wxLogin");
+
+const user2session = (params) => wxRequest(params, apiMall + "/emall/api/wechat/user2session?jsoncallback=?");
+
+
+//查询菜谱列表
+const getRecipes = (params) => wxRequest(params, API + "/recipes");
+
+// 查询菜谱
+const getRecipe = (params) => wxRequest(params, API + "/recipe");
+
+// 查询食物，模糊查询
+const getFoods = (params) => wxRequest(params, API + "/foods");
+
+// 获取常见慢性病菜谱
+const getRecommendRecipes = (params) => wxRequest(params, API + "/recipes/getRecipesByCategory");
+
+const getImages = (params) => wxRequest(params, API + "/getImg");
+
+// 身体信息填写
+const userBodyCheck = (params) => wxRequest(params, API + "/bodyCheck");
+
+export default {
+  getRecipes,
+  getRecipe,
+  getFoods,
+  wxJsCode2Session,
+  getRecommendRecipes,
+  getImages,
+  userBodyCheck
+}
---
+++ src/utils/constant.js
@@ -0,0 +1,26 @@
+/**
+ * 用户code 换取 session_key
+ * @type {String}
+ */
+export const USER_SPECICAL_INFO = "userSpecialInfo";
+
+export const NAME = "name";
+
+export const OPEN_ID = "openId";
+
+/**
+ * 用户信息
+ * @type {String}
+ */
+export const USER_INFO = "userInfo";
+
+/**
+ * 系统信息
+ * @type {String}
+ */
+export const SYSTEM_INFO = "systemInfo";
+
+
+export const ADDRESS_ID = "addressId";
+
+export const SEL_CLASS_CODE = "selClassCode";
